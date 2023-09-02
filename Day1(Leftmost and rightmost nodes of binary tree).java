a47bf54549677b6ceae7392fe78e2ba80d1c972c
Anushka gupta <104565993+Anushkag031@users.noreply.github.com>
2023-09-02 19:11:06 +0530

    Update Day1(Leftmost and rightmost nodes of binary tree).java

--- Day1(Leftmost and rightmost nodes of binary tree).java
+++ Day1(Leftmost and rightmost nodes of binary tree).java
@@ -1,1 +1,28 @@
-
+problem link: https://practice.geeksforgeeks.org/problems/leftmost-and-rightmost-nodes-of-binary-tree/1
+class Tree
+{
+    public static void printCorner(Node node)
+    {
+        
+        // add your code here  
+        if(node==null) return;
+        Queue<Node> q=new LinkedList<>();
+        q.add(node);
+        
+        while(q.isEmpty()==false){
+            int n=q.size();
+            
+            for(int i=1; i<=n; i++){
+                Node curr=q.poll();
+                if(i==1||i==n){    //adding first and last element of the queue
+                    System.out.print(curr.data+" ");
+                }
+                if(curr.left!=null)
+                q.add(curr.left);
+                if(curr.right!=null)
+                q.add(curr.right);
+            }
+        }
+    }
+    
+}
