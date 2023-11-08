527bdeb64aab1ce2e4dba00e8e71f06c360a5416
Anushka gupta <104565993+Anushkag031@users.noreply.github.com>
2023-11-08 16:53:37 +0530

    Create print-mat-in-snake-pattern.java

---
+++ print-mat-in-snake-pattern.java
@@ -0,0 +1,25 @@
+class Solution
+{
+    //Function to return list of integers visited in snake pattern in matrix.
+    static ArrayList<Integer> snakePattern(int matrix[][])
+    {
+        // code here 
+        int n=matrix.length;
+        int m=matrix[0].length;
+        ArrayList<Integer> al=new ArrayList<Integer>();
+        for(int i=0; i<n; i++)
+        {
+            if(i%2==0)
+            for(int j=0; j<m; j++)
+            {
+                al.add(matrix[i][j]);
+            }
+            else
+            for(int j=m-1; j>=0; j--)
+            {
+                al.add(matrix[i][j]);
+            }
+        }
+        return al;
+    }
+}
